e0beff5da3b41dcc8b28d5b017b4be9db5610b11
Takuma Yamada <takuma.yamada22@gmail.com>
2026-09-25 21:39:28 +0900

    Bump versionName to 1.0.2 (versionCode 3) for Play Console release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "1.0.0"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
